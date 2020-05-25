0985aec35d9acd9919515c1905d753cb52cc98b0
Huy Hoang <hoanghuy27297@gmail.com>
2020-05-25 18:57:05 +1000

    initial commit

---
+++ FunnyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunnyQuiz
+//
+//  Created by Huy Hoang on 25/5/20.
+//  Copyright © 2020 Huy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
