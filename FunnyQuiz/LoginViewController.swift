ea7dfa30d4a500793092a12dce2824e195418d56
Huy Hoang <hoanghuy27297@gmail.com>
2020-05-26 18:44:11 +1000

    create interface

--- FunnyQuiz/LoginViewController.swift
+++ FunnyQuiz/LoginViewController.swift
@@ -90,4 +90,15 @@ extension UIView {
             layer.masksToBounds = newValue > 0
         }
     }
+    func dropShadow(scale: Bool = true) {
+      layer.masksToBounds = false
+      layer.shadowColor = UIColor.black.cgColor
+      layer.shadowOpacity = 0.3
+      layer.shadowOffset = CGSize(width: 1, height: 0)
+      layer.shadowRadius = 5
+
+      layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+      layer.shouldRasterize = true
+      layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
 }
