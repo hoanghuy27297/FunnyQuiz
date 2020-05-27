64bb9cf5304cbc0ba855ad051f316851a3fc0e94
Huy Hoang <hoanghuy27297@gmail.com>
2020-05-27 17:41:41 +1000

    add instruction action

--- FunnyQuiz/HomeViewController.swift
+++ FunnyQuiz/HomeViewController.swift
@@ -29,6 +29,34 @@ class HomeViewController: UIViewController {
         performSegue(withIdentifier: "unwindSegueToLogin", sender: self)
     }
 
+    @IBAction func intructionBtn(_ sender: Any) {
+        let alertController = UIAlertController(title: "Intruction", message: "Choose an intruction area to know more about how to do the quiz", preferredStyle: .actionSheet)
+
+        let mathAction = UIAlertAction(title: "Math", style: .default, handler: {(action) -> Void in let alertController = UIAlertController(title: "Math intruction", message: "To do the math quizzes, you must fill in your answers in the text field next to the question. Goodluck!", preferredStyle: .alert)
+            let acceptAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(acceptAction)
+            self.present(alertController, animated: true, completion: nil)
+        })
+        let geoAction = UIAlertAction(title: "Geography", style: .default, handler: {(action) -> Void in let alertController = UIAlertController(title: "Geography intruction", message: "To do the geography quizzes, read carefully the question and options of answers. Choose your answer by click to the button and click next to go to the next question. You are also able to go back to previous answer if you want to change it. Goodluck!", preferredStyle: .alert)
+            let acceptAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(acceptAction)
+            self.present(alertController, animated: true, completion: nil)
+        })
+        let literAction = UIAlertAction(title: "Literature", style: .default, handler: {(action) -> Void in let alertController = UIAlertController(title: "Literature intruction", message: "To do the literature quizzes, read carefully the question and options of answers. Choose your answer by click to the button and click next to go to the next question. You are also able to go back to previous answer if you want to change it. Goodluck!", preferredStyle: .alert)
+            let acceptAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(acceptAction)
+            self.present(alertController, animated: true, completion: nil)
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        alertController.addAction(mathAction)
+        alertController.addAction(geoAction)
+        alertController.addAction(literAction)
+        alertController.addAction(cancelAction)
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
